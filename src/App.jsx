309bd72235e83ed75593ec01f14c07e0a1e13bb9
santosh <sk8065788@gmail.com>
2025-02-20 16:34:50 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <hr />
+    </>
+  )
+}
+
+export default App
